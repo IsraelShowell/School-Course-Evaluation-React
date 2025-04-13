29a9a2827841afa8a1b9c054e5819245dc0a1146
Israel Showell <showellisrael0@gmail.com>
2025-04-13 15:27:51 -0400

    Version: 0.1.0

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
   return (
     <div>
       <h1>Members List</h1>
-      {typeof data.members === "undefined" ? (
+      {typeof data.members === "undfined" ? (
         <p>Loading...</p>
       ) : (
         data.members.map((member, index) => <p key={index}> {member} </p>)
